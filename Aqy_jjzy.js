72e2b522cc15bfd66543a0b9f395dc0ab93f7d8d
aqy_jjzy <100825908+luckyboy5@users.noreply.github.com>
2025-11-19 21:09:12 +0800

    Update Aqy_jjzy.js

--- Aqy_jjzy.js
+++ Aqy_jjzy.js
@@ -5,7 +5,7 @@
     {
       "name": "爱奇艺尖叫之夜抢购",
       "type": "response",
-      "url": "https://raw.githubusercontent.com/luckyboy5/Qx_aqyjjzy/blog/main/Aqy_jjzy.js",
+      "url": "https://raw.githubusercontent.com/luckyboy5/Qx_aqyjjzy/refs/heads/main/Aqy_jjzy.js",
       "match": "^https?:\/\/.*\.iqiyi\.com\/.*(activity|gift|skActivity).*",
       "enabled": true
     }
